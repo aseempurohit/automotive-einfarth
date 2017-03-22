47d54ff6a775b86da5c029644242d720b2e5b48c
Anderson Miller <anderson.miller@frogdesign.com>
2017-03-22 13:27:03 +0100

    deployment to slow.secret.equipment

---
+++ deployment.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import os
+import json
+from datetime import datetime
+from fabric.api import env
+from fabric.api import run
+from fabric.api import local
+from fabric.contrib.files import exists
+from fabric.context_managers import cd
+from fabric.operations import put
+from fabric.operations import get
+from fabric.operations import sudo
+
+
+def deployDocker():
+    run("rm -rf /home/ubuntu/car-network")
+    run("mkdir -p /home/ubuntu/car-network")
+    put("Dockerfile.traditional", "/home/ubuntu/car-network/")
+    put("Makefile", "/home/ubuntu/car-network/")
+    put("lib", "/home/ubuntu/car-network/")
+    put("Car*", "/home/ubuntu/car-network/")
+    with("cd /home/ubuntu/car-network"):
+        sudo("make build")
+        sudo("make run")
+
+
