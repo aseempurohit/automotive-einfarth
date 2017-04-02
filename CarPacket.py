5024e101fe4231d3e666bba084102f3813400453
Anderson Miller <anderson.miller@frogdesign.com>
2017-04-02 23:49:29 +0200

    cleaning up the car packet object, adding version byte

--- CarPacket.py
+++ CarPacket.py
@@ -1,37 +1,49 @@
 import struct
 from random import randint
-
+from parse import parse
 
 class CarPacket(object):
-    def __init__(self, value1, edge1, speed1, dist1, message_id1):
+    def __init__(self, value1, speed1, dist1, message_id1, edge1, version1=0):
         self.analog = int(value1)
-        self.message_id = int(message_id1)
-        self.edge = bool(edge1)
         self.speed = int(speed1)
         self.distance = int(dist1)
+        self.message_id = int(message_id1)
+        self.edge = bool(edge1)
+        self.version = int(version1)
 
     def asBytes(self):
         analog_bytes = struct.pack(">I", self.analog)
+        edge_byte = struct.pack(">B", self.edge)
+        
         try:
             speed_bytes = struct.pack(">I", self.speed)
         except Exception:
             speed_bytes = struct.pack(">I", 0)
+            
         distance_bytes = struct.pack(">I", self.distance)
+        
         message_bytes = struct.pack(">I", self.message_id)
-        edge_byte = struct.pack(">B", self.edge)
+        
+        version_byte = struct.pack(">b", self.version)
         all_bytes = analog_bytes
         all_bytes += speed_bytes
         all_bytes += distance_bytes
         all_bytes += message_bytes
         all_bytes += edge_byte
+        all_bytes += version_byte
         return all_bytes
 
+    def simpleString(self):
+        fmtstring = CarPacket.format_string()
+        return fmtstring.format(self.analog, self.speed, self.distance, self.message_id, self.edge, self.version)
+
     def toString(self):
-      return "[CarPacket] value: {0}, message_id: {1}, edge: {2}, speed: {3}, distance: {4}".format(self.analog,
+        return "[CarPacket] value: {0}, message_id: {1}, edge: {2}, speed: {3}, distance: {4}, v {5}".format(self.analog,
                                                                 self.message_id,
                                                                 self.edge,
                                                                 self.speed,
-                                                                self.distance)
+                                                                self.distance,
+                                                                self.version)
 
     @staticmethod
     def size():
@@ -39,16 +51,28 @@ class CarPacket(object):
         return len(a.asBytes())
 
     @staticmethod
+    def format_string():
+        return "{0}:{1}:{2}:{3}:{4}:{5}"
+
+    @staticmethod
     def fromBytes(value):
         cps = CarPacket.size()
-        if( len(value) != cps ):
+        if len(value) != cps:
             print("got the wrong number of bytes {0} instead of {1}".format(len(value),cps))         
-        value1 = struct.unpack(">I", value[0:4])[0]
-        value2 = struct.unpack(">I", value[4:8])[0]
-        value3 = struct.unpack(">I", value[8:12])[0]
-        value4 = struct.unpack(">I", value[12:16])[0]
-        value5 = bool(struct.unpack(">B", value[16:17])[0])
-        return CarPacket(int(value1), bool(value5), int(value2), int(value3), int(value4))
+        analog = struct.unpack(">I", value[0:4])[0]
+        speed = struct.unpack(">I", value[4:8])[0]
+        distance = struct.unpack(">I", value[8:12])[0]
+        messageid = struct.unpack(">I", value[12:16])[0]
+        edge = bool(struct.unpack(">b", value[16:17])[0])
+        version = int(struct.unpack(">b", value[17:18])[0])
+        return CarPacket(int(analog), int(speed), int(distance), int(messageid), bool(edge), int(version))
+
+    @staticmethod
+    def fromSimpleString(value):
+        fmtstring = CarPacket.format_string()
+        v = parse(fmtstring, value)
+        return CarPacket(v[0], v[1], v[2], v[3], v[4], v[5])
+
 
 
 if __name__ == "__main__":
@@ -57,12 +81,15 @@ if __name__ == "__main__":
     s = randint(0, 100)
     d = randint(0, 100)
     m = randint(0, 100)
-    p = CarPacket(v, e, s, d, m)
+    p = CarPacket(v, s, d, m, e)
     print(p.toString())
     p1 = p.asBytes()
     p2 = CarPacket.fromBytes(p1)
     print(p2.toString())
     print("size of packet is {0} bytes".format(CarPacket.size()))
+    s1 = p2.simpleString()
+    print(s1)
+    print(CarPacket.fromSimpleString(s1).toString())
 
 
 
