55c904cd67a2aa57c738c257943c1966fe534850
Anderson Miller <anderson.miller@frogdesign.com>
2017-04-09 16:59:56 +0200

    make sure the fromBytesToString method is in

--- CarServer.py
+++ CarServer.py
@@ -15,10 +15,18 @@ from time import sleep
 class CarServer(SimpleServer):
     def __init__(self, port2=5002):
         super(CarServer, self).__init__(port1=port2)
+        self.port = port2
+        self.packetSize = CarPacket.size()
         if os.getenv("USE_REDIS"):
             if os.getenv("USE_REDIS").find("TRUE") > -1:
                 self.initRedis('localhost', 'car_value')
-
+    
+    def fromBytesToString(self, value):
+        a = CarPacket.fromBytes(value)
+        s1 = a.simpleString()
+        return s1
+    
+    
     def addClient(self, connection, address1):
         if self.listener is None:
             self.clients.append(CarRecipient(connection, address1))
