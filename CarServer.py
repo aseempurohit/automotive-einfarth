99641e5050eba416f2fdf24683c5b5102d1c0698
Anderson Miller <anderson.miller@frogdesign.com>
2017-04-10 14:35:10 +0200

    subclassing carserver into carserverens

--- CarServer.py
+++ CarServer.py
@@ -1,9 +1,9 @@
 
 import os
 import socket
+import logging
 import sys
 sys.path.append('lib')
-sys.path.append('lib/lib')
 
 from lib.SimpleServer import SimpleServer
 from lib.BroadcastRecipient import BroadcastRecipient
@@ -18,9 +18,36 @@ class CarServer(SimpleServer):
         super(CarServer, self).__init__(port1=port2)
         self.port = port2
         self.packetSize = CarPacket.size()
-        if os.getenv("USE_REDIS"):
+        if os.getenv("USE_REDIS") and not os.getenv("REDIS_SERVICE_HOST"):
             if os.getenv("USE_REDIS").find("TRUE") > -1:
                 self.initRedis('localhost', 'car_value')
+                
+        redis_host = os.getenv('REDIS_SERVICE_HOST')
+
+        if not redis_host:
+            logging.error("REDIS_SERVICE_HOST not set")
+            sys.exit(1)
+
+        redis_port = 6397
+        try:
+            redis_port = int(os.getenv('REDIS_SERVICE_PORT'))
+        except TypeError:
+            logging.error("REDIS_SERVICE_PORT not available")
+
+        if redis_host is None:
+            print "REDIS_SERVICE_HOST not available"
+            sys.exit(1)
+
+        if os.getenv("USE_REDIS"):
+            if os.getenv("USE_REDIS").find("TRUE") > -1:
+                self.initRedis(redis_host, 'car_value', redis_port)
+                logging.info("attempting to use redis with host %s and port %d watching value %s",redis_host,redis_port,'car_value')
+            else:
+                logging.error("not using redis, USE_REDIS != TRUE")
+        else:
+            logging.error("not using redis - USE_REDIS not set")
+            
+            
     
     def fromBytesToString(self, value):
         if type(value) == bytes:
--- CarServerENS.py
+++ CarServerENS.py
@@ -8,55 +8,18 @@ from lib.SimpleServer import SimpleServer
 from lib.BroadcastRecipient import BroadcastRecipient
 from CarPacket import CarPacket
 from CarRecipient import CarRecipient
+from CarServer import CarServer
 
 
 logging.basicConfig(level=logging.DEBUG,
                         format='%(asctime)-15s %(levelname)-8s %(filename)-16s %(lineno)4d %(message)s')
 
-class CarServerENS(SimpleServer):
-    def __init__(self, port2=5002):
-        super(CarServerENS, self).__init__(port1=port2)
-        self.port = port2
+class CarServerENS(CarServer):
+    def __init__(self, port3=5002):
+        super(CarServerENS, self).__init__(port2=port3)
+        self.port = port3
         self.packetSize = CarPacket.size()
 
-        redis_host = os.getenv('REDIS_SERVICE_HOST')
-
-        if not redis_host:
-            logging.error("REDIS_SERVICE_HOST not set")
-            sys.exit(1)
-
-        redis_port = 6397
-        try:
-            redis_port = int(os.getenv('REDIS_SERVICE_PORT'))
-        except TypeError:
-            logging.error("REDIS_SERVICE_PORT not available")
-
-        if redis_host is None:
-            print "REDIS_SERVICE_HOST not available"
-            sys.exit(1)
-
-        if os.getenv("USE_REDIS"):
-            if os.getenv("USE_REDIS").find("TRUE") > -1:
-                self.initRedis(redis_host, 'car_value', redis_port)
-                logging.info("attempting to use redis with host %s and port %d watching value %s",redis_host,redis_port,'ar_value')
-            else:
-                logging.error("not using redis, USE_REDIS != TRUE")
-        else:
-            logging.error("not using redis - USE_REDIS not set")
-
-    def fromBytesToString(self, value):
-        if type(value) == bytes:
-            a = CarPacket.fromBytes(value)
-            s1 = a.simpleString()
-            return s1
-        if type(value) == str:
-            return value
-
-    def addClient(self, connection, address1):
-        if self.listener is None:
-            self.clients.append(CarRecipient(connection, address1))
-        if self.listener is not None:
-            self.listener.addClient(CarRecipient(connection, address1))
 
 
 if __name__ == "__main__":
